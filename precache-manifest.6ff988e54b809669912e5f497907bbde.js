d39354431cfd2d0f457530ccbfff15283ae57027
Anita <24868560+g-anita@users.noreply.github.com>
2018-12-19 22:39:51 -0500

    Updates

---
+++ precache-manifest.6ff988e54b809669912e5f497907bbde.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc884f14d85a35ea3dce",
+    "url": "/react-todo-app/static/js/runtime~main.dc884f14.js"
+  },
+  {
+    "revision": "750c497a4a82fbb83334",
+    "url": "/react-todo-app/static/js/main.750c497a.chunk.js"
+  },
+  {
+    "revision": "9e6fdc68cd37f7ac8486",
+    "url": "/react-todo-app/static/js/1.9e6fdc68.chunk.js"
+  },
+  {
+    "revision": "750c497a4a82fbb83334",
+    "url": "/react-todo-app/static/css/main.6d2a249b.chunk.css"
+  },
+  {
+    "revision": "f845d5055a4e4a36302721238f7c9bbe",
+    "url": "/react-todo-app/index.html"
+  }
+];
